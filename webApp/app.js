f0c5f65e673e6963df6d2e2c6de744fb71a80a84
Lakshay Kumar <lakshayk12@yahoo.com>
2022-02-17 01:06:21 +0530

    Chained post-get async

--- webApp/app.js
+++ webApp/app.js
@@ -40,3 +40,48 @@ getData("/fetch")
     .then((data) => {
         postData('add', { animal: data.animal });
     });
+
+
+// Async POST
+const postData = async (url = '', data = {}) => {
+
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log("error", error);
+    }
+};
+
+// Async GET
+const retrieveData = async (url = '') => {
+    const request = await fetch(url);
+    try {
+        // Transform into JSON
+        const allData = await request.json()
+    }
+    catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+};
+
+// Chain your async functions to post an animal then GET the resulting data
+const postGet = () => {
+    postData('/animal', { fav: 'lion' })
+        .then((data) => {
+            retrieveData('all');
+        })
+}
+
+// Call the chained function
+postGet();
